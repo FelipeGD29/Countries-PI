7a20913fb1b4ab7d95d490ad9bbf82671ba576ed
FelipeGonzalezD29 <gdfeli05@gmail.com>
2024-02-01 23:24:17 -0300

    Deploy intento 2

--- server/src/db.js
+++ server/src/db.js
@@ -5,13 +5,26 @@ const ActivityModel = require("./models/Activity");
 
 const fs = require("fs");
 const path = require("path");
-const { DB_USER, DB_PASSWORD, DB_HOST } = process.env;
+const { DB_USER, DB_PASSWORD, DB_HOST, DB_DEPLOY } = process.env;
 
+// const sequelize = new Sequelize(
+//   `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/countries`,
+//   {
+//     logging: false,
+//     native: false,
+//   },
+  
+// );
 const sequelize = new Sequelize(
-  `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/countries`,
+  DB_DEPLOY,
   {
     logging: false,
     native: false,
+    dialectOptions:{
+      ssl: {
+        require: true
+      }
+    }
   },
   
 );
